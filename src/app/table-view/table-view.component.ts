29535b99f52034adcf1bd33aaa667287afff3cdf
Frédéric Penel <frederic.penel@gmx.fr>
2023-02-06 21:36:29 +0100

    wip

--- src/app/table-view/table-view.component.ts
+++ src/app/table-view/table-view.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable'
+import { map } from 'rxjs';
+import { EntryService } from '../entry.service';
 
 @Component({
   selector: 'app-table-view',
@@ -6,5 +10,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./table-view.component.scss']
 })
 export class TableViewComponent {
+  constructor(private service: EntryService) {
+  }
 
+  displayedColumns: string[] = ['id'];
+
+  getEntries() {
+    return this.service.get().pipe(map(datas => datas.sort((a, b) => a.date.valueOf() - b.date.valueOf())))
+  }
+
+  savePDF() {
+    let doc = new jsPDF();
+
+    autoTable(doc, { html: '#content' })
+    doc.save('table.pdf')
+  }
 }
