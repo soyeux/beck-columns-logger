a151eea7a2d09c379a00e3b048d6b7b40e2b533a
Frédéric Penel <frederic.penel@gmx.fr>
2023-02-06 21:55:22 +0100

    wip

--- src/app/table-view/table-view.component.ts
+++ src/app/table-view/table-view.component.ts
@@ -13,16 +13,26 @@ export class TableViewComponent {
   constructor(private service: EntryService) {
   }
 
-  displayedColumns: string[] = ['id'];
+  displayedColumns: string[] = 
+  ['id', 'date', 'situation', 'emotion', 'autoThough',
+  'behavior', 'alternativeThough', 'consequences'];
 
   getEntries() {
     return this.service.get().pipe(map(datas => datas.sort((a, b) => a.date.valueOf() - b.date.valueOf())))
   }
 
   savePDF() {
-    let doc = new jsPDF();
+    let doc = new jsPDF('landscape');
 
-    autoTable(doc, { html: '#content' })
+    autoTable(doc, { 
+      html: '#content',
+      styles: {
+        fontSize: 8,
+        minCellWidth: 8,
+        cellPadding: 2,
+      },
+      margin: { vertical: 0, horizontal: 0, }, 
+    })
     doc.save('table.pdf')
   }
 }
